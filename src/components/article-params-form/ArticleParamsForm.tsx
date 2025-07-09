b0cb48a0a44509423d1474a5b1ec8f61f61c4f23
Anastasia Egorova <air-neba@yandex.ru>
2025-07-09 11:07:44 +0300

    компонент App вынесен в отдельный файл, переименованы костанты

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,8 +1,6 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { SyntheticEvent, useRef, useState } from 'react';
-import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
-
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
 import {
@@ -18,6 +16,7 @@ import {
 import { RadioGroup } from 'src/ui/radio-group';
 import { Select } from 'src/ui/select';
 import { Text } from 'src/ui/text';
+import { useClose } from 'src/ui/select/hooks/useClose';
 
 type ArticleParamsFormProps = {
 	setArticleState: (param: ArticleStateType) => void;
@@ -28,17 +27,20 @@ export const ArticleParamsForm = ({
 	articleState,
 	setArticleState,
 }: ArticleParamsFormProps) => {
+	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+	const rootRef = useRef<HTMLElement>(null);
+	const [menuState, setMenuState] =
+		useState<ArticleStateType>(defaultArticleState);
 
-	const [isOpen, setIsOpen] = useState<boolean>(false);
-	const rootRef = useRef<HTMLDivElement>(null);
-	const [menuState, setMenuState] = useState<ArticleStateType>(defaultArticleState);
-
-	const handleChangeParam = (key: keyof ArticleStateType, value: OptionType) => {
+	const handleChangeParam = (
+		key: keyof ArticleStateType,
+		value: OptionType
+	) => {
 		setMenuState({ ...articleState, [key]: value });
 	};
 
 	const handleToggleMenu = () => {
-		setIsOpen((IsOpen) => !IsOpen);
+		setIsMenuOpen((isMenuOpen) => !isMenuOpen);
 	};
 
 	const handleApply = (evt: SyntheticEvent) => {
@@ -51,59 +53,56 @@ export const ArticleParamsForm = ({
 		setArticleState(defaultArticleState);
 	};
 
-	useOutsideClickClose({
-		isOpen,
+	useClose({
+		isOpen: isMenuOpen,
+		onClose: () => setIsMenuOpen(false),
 		rootRef,
-		onClose: () => setIsOpen(false),
-		onChange: setIsOpen,
+		onChange: setIsMenuOpen,
 	});
 
 	return (
 		<>
-			<ArrowButton
-				isOpen={isOpen}
-				onClick={handleToggleMenu}
-			/>
+			<ArrowButton isOpen={isMenuOpen} onClick={handleToggleMenu} />
 			<aside
 				ref={rootRef}
-				className={clsx(styles.container, isOpen && styles.container_open)}
-				style={{ display: isOpen ? 'block' : 'none' }}>
+				className={clsx(styles.container, isMenuOpen && styles.container_open)}>
 				<form
 					className={styles.form}
 					onSubmit={handleApply}
-					onReset={handleReset}
-					>
-					<Text size={31} uppercase weight={800}>Задайте параметры</Text>
+					onReset={handleReset}>
+					<Text size={31} uppercase weight={800}>
+						Задайте параметры
+					</Text>
 					<Select
 						options={fontFamilyOptions}
 						selected={menuState.fontFamilyOption}
 						title='Шрифт'
-						onChange={(option)=>handleChangeParam('fontFamilyOption', option)}
+						onChange={(option) => handleChangeParam('fontFamilyOption', option)}
 					/>
 					<RadioGroup
 						options={fontSizeOptions}
 						selected={menuState.fontFamilyOption}
 						title='Размер шрифта'
 						name='fontSize'
-						onChange={(option)=>handleChangeParam('fontFamilyOption', option)}
+						onChange={(option) => handleChangeParam('fontFamilyOption', option)}
 					/>
 					<Select
 						options={fontColors}
 						selected={menuState.fontColor}
 						title='Цвет шрифта'
-						onChange={(option)=>handleChangeParam('fontColor', option)}
+						onChange={(option) => handleChangeParam('fontColor', option)}
 					/>
 					<Select
 						options={backgroundColors}
 						selected={menuState.backgroundColor}
 						title='Цвет фона'
-						onChange={(option)=>handleChangeParam('backgroundColor', option)}
+						onChange={(option) => handleChangeParam('backgroundColor', option)}
 					/>
 					<Select
 						options={contentWidthArr}
 						selected={menuState.contentWidth}
 						title='Ширина контена'
-						onChange={(option)=>handleChangeParam('contentWidth', option)}
+						onChange={(option) => handleChangeParam('contentWidth', option)}
 					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
